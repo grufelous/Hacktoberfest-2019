281806e46a52274c17cb3bc965f8d876e9bf52f5
Pratik Anand <grufelous@gmail.com>
2019-10-05 22:16:35 +0530

    Added menubar for site

---
+++ codevu-site/src/components/MenuBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="menu">
+        <ul>
+            <li>Home</li>
+            <li>Learn</li>
+            <li>Explore</li>
+            <li>Contribute</li>
+        </ul>
+    </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'menu',
+  components: {
+    
+  },
+}
+</script>
+
+<style scoped>
+
+ul {
+    text-decoration: none;
+}
+li {
+    /* background: #efefef; */
+    display: inline-block;
+    padding: 1.5em 1.0em;
+    min-width: 6em;
+    margin: 0 0.5em;
+    border-radius: 40%;
+    transition: all 0.2s ease-in;
+    cursor: pointer;
+}
+li:hover {
+    background: #fafafa;
+    display: inline-block;
+    min-width: 5.5em;
+    margin: 0 0.5em;
+    border-radius: 40%;
+}
+</style>

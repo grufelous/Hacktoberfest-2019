eb0e56003065a96cdb4feef85c0431a9a7983383
VaibhavBansal93 <36199233+VaibhavBansal93@users.noreply.github.com>
2019-10-15 21:27:58 +0530

    Delete ReverseInteger.py

--- problems/beginner/ReverseInteger.py
+++
@@ -1,6 +0,0 @@
-input_integer = int(input("Enter the number : "))
-reversed_integer = 0
-while input_integer != 0:
-    reversed_integer = reversed_integer*10 + input_integer%10
-    input_integer //= 10
-print(reversed_integer)
